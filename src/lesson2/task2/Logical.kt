2af817c7996250369819cdb54c5b46d59304065b
dmitriikuzmin <dkuzmin200041@gmail.com>
2018-09-26 23:14:37 +0300

    Решен второй урок v 1.1

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -17,10 +17,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = when {
-    (number / 1000 + (number / 100 % 10)) == (number / 10 % 10 + number % 10) -> true
-    else -> false
-}
+fun isNumberHappy(number: Int): Boolean = (number / 1000 + (number / 100 % 10)) == (number / 10 % 10 + number % 10)
 
 /**
  * Простая
@@ -29,12 +26,7 @@ fun isNumberHappy(number: Int): Boolean = when {
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
-    return when {
-        x1 == x2 || y1 == y2 || kotlin.math.abs(x1 - x2) == kotlin.math.abs(y1 - y2) -> return true
-        else -> false
-    }
-}
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = x1 == x2 || y1 == y2 || kotlin.math.abs(x1 - x2) == kotlin.math.abs(y1 - y2)
 
 /**
  * Простая
@@ -59,12 +51,7 @@ fun daysInMonth(month: Int, year: Int): Int {
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean {
-    return when {
-        (kotlin.math.sqrt(sqr(x2 - x1) + sqr(y2 - y1)) + r1) <= r2 -> true
-        else -> false
-    }
-}
+                 x2: Double, y2: Double, r2: Double): Boolean = ((kotlin.math.sqrt(sqr(x2 - x1) + sqr(y2 - y1)) + r1) <= r2)
 
 /**
  * Средняя
@@ -75,8 +62,5 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = when {
-    (a <= r && b <= s) || (a <= s && b <= r) || (a <= r && c <= s) || (a <= s && c <= r) || (b <= r && c <= s) || (b <= s && c <= r) -> true
-    else -> false
-}
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = (a <= r && b <= s) || (a <= s && b <= r) || (a <= r && c <= s) || (a <= s && c <= r) || (b <= r && c <= s) || (b <= s && c <= r)
 
